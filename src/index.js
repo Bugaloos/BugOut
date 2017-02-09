fd86ce2da680c7c6dfb01e77253630bcd4363c59
Bill Burgess <wmhburgess@gmail.com>
2017-02-10 11:14:32 +1300

    adds register form

--- src/index.js
+++ src/index.js
@@ -12,8 +12,11 @@ const MuiThemeProvider = require('material-ui/styles')
 // Top Level Components
 const App = require('./containers/app')
 const Home = require('./containers/home')
+
+// teir 2 components
 const Users = require('./components/users')
 const Login = require('./components/login')
+const Register = require('./components/register')
 
 
 const store = createStore(reducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
@@ -29,7 +32,8 @@ const Root = ({store}) => {
           <Route path='/' component={App}>
             <IndexRoute component={Home} />
             <Route path='/users' component={Users} />
-            <Route path='/login' component={Login} />
+            <Route path='/login' component={Login} /> //festering
+            <Route path='/register' component={Register} />
           </Route>
         </Router>
       </Provider>

44f65adbec372e3ffc4873ee2ae044cc447bc8da
james-brown98 <james.brown9809@hotmail.co.nz>
2017-02-10 12:30:02 +1300

    fixes pouchdb bug

--- src/containers/group.js
+++ src/containers/group.js
@@ -1,12 +1,19 @@
 const React = require('react')
 const { connect } = require('react-redux')
+const Messageboard = require('../components/messageboard')
+
 
 function Group(props) {
   return (
     <div>
-      <h1>This is the Group Component </h1>
+      <h1>This is the Group Component</h1>
+      <p>This is the main description of the plan for the group</p>
+      <Messageboard router={props.router} />
     </div>
   )
 }
+
+
+
 
 module.exports = connect((state) => state)(Group)

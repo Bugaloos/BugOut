b64009a370a4811bffc14d1059eeefee2d22fa84
james-brown98 <james.brown9809@hotmail.co.nz>
2017-02-10 14:05:01 +1300

    pulling

--- src/components/messageboard.js
+++ src/components/messageboard.js
@@ -4,7 +4,27 @@ const { connect } = require('react-redux')
 function Messageboard(props) {
   return (
     <div>
-      <h1>This is the container Messageboard component</h1>
+        <div>
+            <div>
+                <span>Comments</span>
+                <ul></ul>
+            </div>
+            <div>
+                <form>
+                    <div>
+                        <input id="nick" name="nick" type="text">
+                        <label>Name</label>
+                    </div>
+                    <div>
+                        <input id="message" name="message" type="text">
+                        <label>Message</label>
+                    </div>
+                    <div>
+                        <button id="add">post</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
   )
 }

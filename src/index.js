a1a5637f1bd517311f3cdbdefecb4a02603371c9
james-brown98 <james.brown9809@hotmail.co.nz>
2017-02-10 15:02:03 +1300

    rough message board functions

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const createHistory = require('history').createHashHistory
 const { Router, Route, IndexRoute, hashHistory } = require('react-router')
 const reducer = require('./reducers')
 const initialState = require('../state')
-const MuiThemeProvider = require('material-ui/styles')
+const MuiThemeProvider = require('material-ui/styles/MuiThemeProvider').default
 
 // Top Level Components
 const App = require('./containers/app')
@@ -14,6 +14,10 @@ const Home = require('./containers/home')
 const Users = require('./components/users')
 const Login = require('./components/login')
 const Register = require('./components/register')
+const Profile = require('./containers/profile')
+const Group = require('./containers/group')
+const Plan = require('./components/plan')
+// const Messageboard = require('./components/messageboard')
 
 const store = createStore(reducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
@@ -22,29 +26,32 @@ store.subscribe(() => {
 })
 
 const Root = ({store}) => {
+
 	return (
-		<Provider store = {store}>
-			<Router history = {hashHistory}>
-				<Route path = '/' component={App}>
-					<IndexRoute component={Home} />
-						<Route path='/login' component={Login} /> //festering
-						<Route path='/register' component={Register} />
+		<MuiThemeProvider>
+			<Provider store = {store}>
+				<Router history = {hashHistory}>
+					<Route path = '/' component={App}>
+						<IndexRoute component={Home} />
+							<Route path='/login' component={Login} /> //festering
+							<Route path='/register' component={Register} />
 
-						<Route path = '/users' component={Users} />
-						<Route path = '/users/:id' component={Profile} />
-						<Route path = '/users/:id/edit' component={Profile} />
+							<Route path = '/users' component={Users} />
+							<Route path = '/users/:id' component={Profile} />
+							<Route path = '/users/:id/edit' component={Profile} />
 
-						<Route path = '/groups' component={Group} />
-						<Route path = '/groups/:id' component={Group} />
-						<Route path = '/groups/:id/new' component={Group} />
-						<Route path = '/groups/:id/edit' component={Group} />
+							<Route path = '/groups' component={Group} />
+							<Route path = '/groups/:id' component={Group} />
+							<Route path = '/groups/:id/new' component={Group} />
+							<Route path = '/groups/:id/edit' component={Group} />
 
-						<Route path = '/plans/:id' component={Plan} />
-						<Route path = '/plans/:id/new' component={Plan} />
-						<Route path = '/plans/:id/edit' component={Plan} />
-				</Route>
-			</Router>
-		</Provider>
+							<Route path = '/plans/:id' component={Plan} />
+							<Route path = '/plans/:id/new' component={Plan} />
+							<Route path = '/plans/:id/edit' component={Plan} />
+					</Route>
+				</Router>
+			</Provider>
+		</MuiThemeProvider>
 	)
 }
 // <MuiThemeProvider>

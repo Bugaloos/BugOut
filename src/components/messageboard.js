2a550b128d96ca30da53b9f33f6b92561590dbe1
james-brown98 <james.brown9809@hotmail.co.nz>
2017-02-10 15:40:14 +1300

    boiled down mui messageboard

--- src/components/messageboard.js
+++ src/components/messageboard.js
@@ -15,157 +15,64 @@ import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 const { connect } = require('react-redux')
 
-const iconButtonElement = (
-  <IconButton
-    touch={true}
-    tooltip="more"
-    tooltipPosition="bottom-left"
-  >
-    <MoreVertIcon color={grey400} />
-  </IconButton>
-);
+const data = {
+  userName: "Lucas",
+  message: "Hello"
+}
 
-const rightIconMenu = (
-  <IconMenu iconButtonElement={iconButtonElement}>
-    <MenuItem>Reply</MenuItem>
-    <MenuItem>Forward</MenuItem>
-    <MenuItem>Delete</MenuItem>
-  </IconMenu>
-);
+//FIX MEEEEEEEEE
+
 
 const Messageboard = () => (
   <div>
       <List>
-        <Subheader>Today</Subheader>
+        <Subheader>Group Message Board</Subheader>
+
         <ListItem
-          leftAvatar={<Avatar src="images/ok-128.jpg" />}
-          primaryText="Brunch this weekend?"
-          secondaryText={
-            <p>
-              <span style={{color: darkBlack}}>Brendan Lim</span> --
-              I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
-            </p>
-          }
-          secondaryTextLines={2}
-        />
-        <Divider inset={true} />
-        <ListItem
-          leftAvatar={<Avatar src="images/kolage-128.jpg" />}
           primaryText={
-            <p>Summer BBQ&nbsp;&nbsp;<span style={{color: lightBlack}}>4</span></p>
-          }
-          secondaryText={
             <p>
-              <span style={{color: darkBlack}}>to me, Scott, Jennifer</span> --
-              Wish I could come, but I&apos;m out of town this weekend.
+              <span style={{color: darkBlack}}>Lucas</span> --
+              I am going to see the fish flapping about at Oriental Bay! Fun!!
             </p>
           }
           secondaryTextLines={2}
         />
         <Divider inset={true} />
         <ListItem
-          leftAvatar={<Avatar src="images/uxceo-128.jpg" />}
-          primaryText="Oui oui"
-          secondaryText={
+          primaryText={
             <p>
-              <span style={{color: darkBlack}}>Grace Ng</span> --
-              Do you have Paris recommendations? Have you ever been?
+              <span style={{color: darkBlack}}>Mischa</span> --
+              Wow I am going to raft down Cuba Street!
             </p>
           }
           secondaryTextLines={2}
         />
         <Divider inset={true} />
         <ListItem
-          leftAvatar={<Avatar src="images/kerem-128.jpg" />}
-          primaryText="Birdthday gift"
-          secondaryText={
+          primaryText={
             <p>
-              <span style={{color: darkBlack}}>Kerem Suer</span> --
-              Do you have any ideas what we can get Heidi for her birthday? How about a pony?
+              <span style={{color: darkBlack}}>Meghan</span> --
+              I am going to go up to the top floor to see how much it shakes in the next quake!
             </p>
           }
           secondaryTextLines={2}
         />
         <Divider inset={true} />
         <ListItem
-          leftAvatar={<Avatar src="images/raquelromanp-128.jpg" />}
-          primaryText="Recipe to try"
-          secondaryText={
+          primaryText={
             <p>
-              <span style={{color: darkBlack}}>Raquel Parrado</span> --
-              We should eat this: grated squash. Corn and tomatillo tacos.
+              <span style={{color: darkBlack}}>Bill</span> --
+              Looting is so fun! Free stuff for everyone!
             </p>
           }
           secondaryTextLines={2}
         />
       </List>
-      <List>
-        <Subheader>Today</Subheader>
-        <ListItem
-          leftAvatar={<Avatar src="images/ok-128.jpg" />}
-          rightIconButton={rightIconMenu}
-          primaryText="Brendan Lim"
-          secondaryText={
-            <p>
-              <span style={{color: darkBlack}}>Brunch this weekend?</span><br />
-              I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
-            </p>
-          }
-          secondaryTextLines={2}
-        />
-        <Divider inset={true} />
-        <ListItem
-          leftAvatar={<Avatar src="images/kolage-128.jpg" />}
-          rightIconButton={rightIconMenu}
-          primaryText="me, Scott, Jennifer"
-          secondaryText={
-            <p>
-              <span style={{color: darkBlack}}>Summer BBQ</span><br />
-              Wish I could come, but I&apos;m out of town this weekend.
-            </p>
-          }
-          secondaryTextLines={2}
-        />
-        <Divider inset={true} />
-        <ListItem
-          leftAvatar={<Avatar src="images/uxceo-128.jpg" />}
-          rightIconButton={rightIconMenu}
-          primaryText="Grace Ng"
-          secondaryText={
-            <p>
-              <span style={{color: darkBlack}}>Oui oui</span><br />
-              Do you have any Paris recs? Have you ever been?
-            </p>
-          }
-          secondaryTextLines={2}
-        />
-        <Divider inset={true} />
-        <ListItem
-          leftAvatar={<Avatar src="images/kerem-128.jpg" />}
-          rightIconButton={rightIconMenu}
-          primaryText="Kerem Suer"
-          secondaryText={
-            <p>
-              <span style={{color: darkBlack}}>Birthday gift</span><br />
-              Do you have any ideas what we can get Heidi for her birthday? How about a pony?
-            </p>
-          }
-          secondaryTextLines={2}
-        />
-        <Divider inset={true} />
-        <ListItem
-          leftAvatar={<Avatar src="images/raquelromanp-128.jpg" />}
-          rightIconButton={rightIconMenu}
-          primaryText="Raquel Parrado"
-          secondaryText={
-            <p>
-              <span style={{color: darkBlack}}>Recipe to try</span><br />
-              We should eat this: grated squash. Corn and tomatillo tacos.
-            </p>
-          }
-          secondaryTextLines={2}
-        />
-      </List>
+      <form>
+        <input type='text' value='comment'/>
+        <input type='submit' value='Post'/>
+      </form>
+
   </div>
 );
 

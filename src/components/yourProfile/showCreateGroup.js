eb594c071f1bf928d4b36848c1f11e3aceaf957b
MeghanHughes <hughes.meghan@gmail.com>
2017-02-14 09:56:00 +1300

    adds console log

--- src/components/yourProfile/showCreateGroup.js
+++ src/components/yourProfile/showCreateGroup.js
@@ -3,12 +3,14 @@ const { connect } = require('react-redux')
 const CreateGroupStepper = require('./createGroupStepper')
 
 function showCreateGroup (props) {
-  const createGroupButton =
-    <button onClick={() => props.dispatch({type: 'TOGGLE_CREATE_GROUP'})}>Create A Group</button>
 
   return props.showCreateGroup
     ? <CreateGroupStepper {...props} />
-    : createGroupButton
+    : <button onClick={() => {
+      console.log("button", props);
+        props.dispatch({type: 'TOGGLE_CREATE_GROUP'})
+      }
+      }>Create A Group</button>
 }
 
 module.exports = connect((state) => state)(showCreateGroup)

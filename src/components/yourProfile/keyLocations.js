c23c3f0df5be59fff1a55b75d8490dd86e178972
Bill Burgess <wmhburgess@gmail.com>
2017-02-15 12:40:44 +1300

    merging

--- src/components/yourProfile/keyLocations.js
+++ src/components/yourProfile/keyLocations.js
@@ -5,9 +5,33 @@ const backButton = require('./backButton')
 
 class KeyLocations extends React.Component {
 
+  componentDidMount () {
+    this.geocoder = new google.maps.Geocoder()
+  }
+
   handleSubmit () {
+    var Latlng = new google.maps.LatLng
+    var swlatlng = {lat: -46.570384, lng: 166.438844}
+    var nelatlng = {lat: -34.085704, lng: 179.127941}
+    var latlngbounds = new google.maps.LatLngBounds(swlatlng, nelatlng)
+    const meetingAddress = this.refs.meetingPoint.input.value
+    const opts = {
+      'address': meetingAddress,
+      'location': Latlng,
+      'bounds': latlngbounds
+    }
+    this.geocoder.geocode(opts, (results, status) => {
+
+      if (status === google.maps.GeocoderStatus.OK) {
+        console.log("this is geocode Latlng", Latlng)
+        console.log('results', results)
+        const viewport = result[0].geometry.viewport
+        const meetingPoint = {lat: viewport.f.b, lng: viewport.b.b}
+        console.log('meetingPoint', meetingPoint);
+      }
+    })
+
     const { dispatch, showingComponent } = this.props
-    const meetingPoint = this.refs.meetingPoint.input.value
     const safePoint = this.refs.safePoint.input.value
 
     if (showingComponent === 'CREATE_GROUP') {

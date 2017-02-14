e7bb910a58afaa00314dd4141f820e290ec461c6
james-brown98 <james.brown9809@hotmail.co.nz>
2017-02-15 10:02:04 +1300

    updates layout slightly

--- src/components/offlineDetails.js
+++ src/components/offlineDetails.js
@@ -12,18 +12,23 @@ class OfflineDetails extends React.Component {
     return (
       <div>
         <h3>Offline Experience</h3>
-        <p>In the case of an Emergency such as a natural disastah, it is possible your device
+
+        <p>In the case of an Emergency such as a natural disaster, it is possible your device
         will lose internet connection.</p>
+
         <p>In such a situation however, you will still be able to access your Individual or Group plans,
         view your safe point, meeting point, and cache locations.
-        You will also be able post to your message board, where your messages will wait until connection is reastablished,
+        </p>
+
+        <p>You will also be able post to your message board, where your messages will wait until connection is reastablished,
         allowing you to keep communicating with patchy internet connection.
         </p>
+
         <form>
           <button onClick={this.handleSubmit.bind(this)}> I get it already </button>
         </form>
       </div>
-      //completeProfile is where OfflineDetails is being rendered for no particular reason. 
+      //completeProfile is where OfflineDetails is being rendered for no particular reason.
     )
   }
 }
